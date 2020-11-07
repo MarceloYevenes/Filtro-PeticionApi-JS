4b1290f385b14a4c59de3468aeb4b0185b5a9208
MarceloYevenes <yevenesmarcelo1@gmail.com>
2020-11-06 22:11:35 -0300

    agregada api en linea

--- js/bd.js
+++ js/bd.js
@@ -3,7 +3,7 @@ let personas = [];
 async function runFetch() {
     try {
         //PETICION A LA API
-        let resultado = await fetch('http://127.0.1.1/usuarios.php', {
+        let resultado = await fetch('https://apipruebamarcelo.000webhostapp.com/usuarios.php', {
             method: 'POST',
         });
         //RESPUESTA DE LA API Y CONVERSION DE LOS DATOS A UN OBJETO
